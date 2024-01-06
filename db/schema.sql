f7a1081cf881218f86edf401c7e1599dd3046160
rnahill <rei333lly@gmail.com>
2024-01-06 16:05:06 -0500

    started to create tables in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,31 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE Category (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    category_name VARCHAR(30) NOT NULL
+);
+
+
+CREATE TABLE Product (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    product_name VARCHAR(30) NOT NULL,
+    price -- decimal, NOT NULL, validation that value is a decimal
+    stock INT NOT NULL -- default value of 10, validates that value is numeric
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+
+CREATE TABLE Tag (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    tag_name VARCHAR(30)
+);
+
+CREATE TABLE ProductTag (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES Product(id)
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
