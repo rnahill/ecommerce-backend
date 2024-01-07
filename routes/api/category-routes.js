01049724d6588addf5ac6fc12a8b5fa3f135c3cf
rnahill <rei333lly@gmail.com>
2024-01-07 15:20:03 -0500

    added express routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,91 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+
+    const categories = await Category.findAll({
+      include: Product,
+    });
+
+    res.status(200).json(categories);
+
+  } catch (err) {
+
+    console.error(err);
+    res.status(500).json({error: "Error"})
+
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+
+    const category = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+
+    res.status(200).json(category);
+
+  } catch (err) {
+
+    console.error(err);
+    res.status(500).json({error: "Error"})
+
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+
+  try {
+
+    const newCategory = await Category.create(req.body);
+
+    res.status(201).json(newCategory);
+
+  } catch (err) {
+
+    console.error(err);
+    res.status(500).json({error: "Error"})
+
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+
+  try {
+     const [updatedRows] = await Category.update(req.body, {
+      where: { id: req.params.id },
+     });
+  }  catch (err) {
+
+    console.error(err);
+    res.status(500).json({error: "Error"})
+
+  }
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+
+  try {
+
+    const deletedCategory = await.destroy({
+      where: { id: req.params.id },
+    });
+
+  }  catch (err) {
+
+    console.error(err);
+    res.status(500).json({error: "Error"})
+
+  }
+
 });
 
 module.exports = router;
